824283f77f84e2e8ce5146ab4c2a33c4efc15bf3
mariasoda <msodano@massart.edu>
2017-05-14 11:20:53 -0400

    homepage

--- work.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$(".imagelink").hover(function(){
-		$(".portfolio").css("opacity", .5;),
-		$(".title").css("font-weight", 600;),
-		$(".title").css("background-color", "#3333ff";)
-	});
-});
